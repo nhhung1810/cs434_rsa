cc425b3a3b0dedffabab10a4b0eed28057238f1c
nhhung1810 <nghung224@gmail.com>
2022-07-04 05:53:24 -0400

    Excercise 2 and 3 commited

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ BIGNUM *make_rsa_private_key(BIGNUM *p, BIGNUM *q, BIGNUM *e)
     return res;
 }
 
-int main()
+void exercise_1()
 {
     // Exercise 1.
     BN_CTX *ctx = BN_CTX_new();
@@ -52,4 +52,62 @@ int main()
     printBN("RSA Private Key", private_key);
 
     BN_CTX_free(ctx);
+}
+
+void exercise_2()
+{
+    // Exercise 2.
+    BN_CTX *ctx = BN_CTX_new();
+
+    BIGNUM *n = BN_new();
+    BIGNUM *e = BN_new();
+    BIGNUM *d = BN_new();
+    BIGNUM *msg = BN_new();
+    BIGNUM *encrypted = BN_new();
+
+    // M = A top secret!
+
+    BN_hex2bn(&n, "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
+    BN_hex2bn(&e, "010001");
+    BN_hex2bn(&d, "74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D");
+    BN_hex2bn(&msg, "4120746f702073656372657421");
+
+    // Encryption (msg ^ e) % n
+    BN_mod_exp(encrypted, msg, e, n, ctx);
+    printBN("Encrypted msg", encrypted);
+
+    BN_CTX_free(ctx);
+}
+
+void exercise_3()
+{
+    // Exercise 3.
+    BN_CTX *ctx = BN_CTX_new();
+
+    BIGNUM *n = BN_new();
+    BIGNUM *e = BN_new();
+    BIGNUM *d = BN_new();
+    BIGNUM *C = BN_new();
+    BIGNUM *decrypted = BN_new();
+
+    // M = A top secret!
+
+    BN_hex2bn(&n, "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
+    BN_hex2bn(&e, "010001");
+    BN_hex2bn(&d, "74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D");
+    BN_hex2bn(&C, "8C0F971DF2F3672B28811407E2DABBE1DA0FEBBBDFC7DCB67396567EA1E2493F");
+
+    // Decryption m = (c ^ d) % n
+    BN_mod_exp(decrypted, C, d, n, ctx);
+    printBN("Decrypted msg", decrypted);
+    // result after convert is Password is dees
+
+    BN_CTX_free(ctx);
+}
+
+int main()
+{
+    exercise_1();
+    exercise_2();
+    exercise_3();
 }
---
+++ make_msg.py
@@ -0,0 +1,3 @@
+msg = "this is Hung"
+
+print(msg.encode(encoding="hex"))
